72a9a9079f1d05e29255aae9f10a712901cc32e4
Timur Badretdinov <accounts@destiner.io>
2024-04-08 18:29:26 +0100

    Use Minio for storage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,47 +1,49 @@
-import {
-  S3Client,
-  PutObjectCommand,
-  GetObjectCommand,
-  S3ServiceException,
-} from '@aws-sdk/client-s3';
+import { Readable } from 'node:stream';
 
-const accountId = process.env.CLOUDFLARE_ACCOUNT_ID as string;
-const accessKeyId = process.env.CLOUDFLARE_ACCESS_KEY_ID as string;
-const secretAccessKey = process.env.CLOUDFLARE_SECRET_ACCESS_KEY as string;
-const bucket = process.env.CLOUDFLARE_R2_BUCKET as string;
+import * as Minio from 'minio';
 
-const s3 = new S3Client({
-  region: 'auto',
-  endpoint: `https://${accountId}.r2.cloudflarestorage.com`,
-  credentials: {
-    accessKeyId,
-    secretAccessKey,
-  },
+const endpoint = process.env.MINIO_PUBLIC_ENDPOINT as string;
+const accessKey = process.env.MINIO_ACCESS_KEY as string;
+const secretKey = process.env.MINIO_SECRET_KEY as string;
+const bucket = process.env.MINIO_BUCKET as string;
+
+const minioClient = new Minio.Client({
+  endPoint: endpoint,
+  port: 443,
+  accessKey,
+  secretKey,
 });
+
+async function streamToString(stream: Readable): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let data = '';
+
+    stream.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    stream.on('end', () => {
+      resolve(data);
+    });
+
+    stream.on('error', (error) => {
+      reject(error);
+    });
+  });
+}
 
 async function getObject(key: string): Promise<string | null> {
   try {
-    const file = await s3.send(
-      new GetObjectCommand({ Bucket: bucket, Key: key }),
-    );
-    const fileBody = file.Body;
-    if (!fileBody) {
-      return null;
-    }
-    const fileString = await fileBody.transformToString();
+    const file = await minioClient.getObject(bucket, key);
+    const fileString = await streamToString(file);
     return fileString;
   } catch (e) {
-    if (e instanceof S3ServiceException) {
-      if (e.name === 'NoSuchKey') {
-        return null;
-      }
-    }
-    throw e;
+    return null;
   }
 }
 
 async function putObject(key: string, body: string): Promise<void> {
-  await s3.send(new PutObjectCommand({ Bucket: bucket, Key: key, Body: body }));
+  await minioClient.putObject(bucket, key, body);
 }
 
 export { getObject, putObject };
