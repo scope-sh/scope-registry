681e6ca7da2c8d943519b581736a72cc1bc3ae72
Timur Badretdinov <accounts@destiner.io>
2024-04-15 15:54:40 +0100

    Extract Rhinestone V1 modules into a separate source

--- src/labels/sources/rhinestone/v1-modules.ts
+++ src/labels/sources/rhinestone/v1-modules.ts
@@ -1,83 +1,38 @@
-import { decodeEventLog, encodeEventTopics } from 'viem';
-import type { Address, Hex } from 'viem';
+import { Address } from 'viem';
 
-import rhinestoneV1RegistryAbi from '@/abi/rhinestoneV1Registry.js';
 import { Source as BaseSource } from '@/labels/base.js';
-import type { ChainLabelMap, LabelMap } from '@/labels/base.js';
-import {
-  getLabelNamespaceByValue,
-  getLabelTypeById,
-  initLabelMap,
-} from '@/labels/utils.js';
+import type { LabelMap } from '@/labels/base.js';
 import { CHAINS } from '@/utils/chains.js';
-import type { ChainId } from '@/utils/chains.js';
-import { getEvents } from '@/utils/fetching.js';
+import { getDeployed } from '@/utils/fetching.js';
 
-interface Module {
-  address: Address;
-  sender: Address;
-  resolver: Hex;
-}
-
-const REGISTRY_ADDRESS = '0xe0cde9239d16bef05e62bbf7aa93e420f464c826';
-const NAMESPACE = 'Rhinestone V1';
+import { toLabelMap } from '../../utils.js';
 
 class Source extends BaseSource {
-  getName(): string {
-    return 'Rhinestone V1 Accounts';
+  override getName(): string {
+    return 'Rhinestone V1 Modules';
   }
 
   async fetch(): Promise<LabelMap> {
-    const labels = initLabelMap();
+    const addresses: Record<string, Record<Address, string>> = {};
+    const labels: Record<Address, string> = {
+      '0xf83d07238a7c8814a48535035602123ad6dbfa63': 'Ownable Validator',
+      '0x1c936be884ce91ecfbdd10c7898c22b473eab81a': 'Webauthn Validator',
+      '0x2f28bcd0f3de8845409b947d9de45ca5ed776767': 'Multi Factor Validator',
+      '0x9fc07b42b4e13cbea040f4f3f80223c9adf4005e': 'Auto Savings Executor',
+      '0x506a89d85a9733225fdb54d9e7e76d017c21e1c1': 'Scheduled Orders Executor',
+      '0xad6330089d9a1fe89f4020292e1afe9969a5a2fc':
+        'Scheduled Transfers Executor',
+      '0xba313f2beee4118b97dd565ed451f722219851be': 'Cold Storage Executor',
+      '0xdfe507ad4035256e586f9e8b7d0e82184d5b8cb4': 'Cold Storage Hook',
+      '0x71181a93753d489a10927fa747eefd8425ff40de': 'Flashloan Callback',
+      '0x10a734a2c140a3d8e7919ad19c4d2f15f2f5f36b': 'Flashloan Lender',
+      '0xc5409e364bed768facd8ce3e534a9c7787c0ec3b': 'Session Key Manager',
+    };
     for (const chain of CHAINS) {
-      const chainLabels = await this.fetchLabels(chain);
-      labels[chain] = chainLabels;
+      const chainAddresses = await getDeployed(chain, labels);
+      addresses[chain] = chainAddresses;
     }
-
-    return labels;
-  }
-
-  private async fetchLabels(chain: ChainId): Promise<ChainLabelMap> {
-    const topics = encodeEventTopics({
-      abi: rhinestoneV1RegistryAbi,
-      eventName: 'ModuleRegistration',
-    });
-    const topic = topics[0];
-    if (!topic) {
-      return {};
-    }
-    const events = await getEvents(chain, REGISTRY_ADDRESS, topic);
-
-    const modules: Module[] = events.map((event) => {
-      const decodedEvent = decodeEventLog({
-        abi: rhinestoneV1RegistryAbi,
-        data: event.data as Hex,
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        topics: event.topics,
-      });
-      if (decodedEvent.eventName !== 'ModuleRegistration') {
-        throw new Error('Invalid event name');
-      }
-      return {
-        address: decodedEvent.args.implementation.toLowerCase() as Address,
-        sender: decodedEvent.args.sender.toLowerCase() as Address,
-        resolver: decodedEvent.args.resolver as Hex,
-      };
-    });
-
-    return Object.fromEntries(
-      modules.map((module) => {
-        return [
-          module.address,
-          {
-            value: 'Module',
-            type: getLabelTypeById('rhinestone-v1-module'),
-            namespace: getLabelNamespaceByValue(NAMESPACE),
-          },
-        ];
-      }),
-    );
+    return toLabelMap(addresses, 'Rhinestone V1', 'erc7579-module');
   }
 }
 
--- src/labels/sources/rhinestone/v1.ts
+++ src/labels/sources/rhinestone/v1.ts
@@ -22,18 +22,6 @@ class Source extends BaseSource {
         'ERC-7579 Reference Singleton (Advanced)',
       '0x5e9f3feec2aa6706df50de955612d964f115523b':
         'ERC-7579 Reference Bootstrap',
-      '0xf83d07238a7c8814a48535035602123ad6dbfa63': 'Ownable Validator',
-      '0x1c936be884ce91ecfbdd10c7898c22b473eab81a': 'Webauthn Validator',
-      '0x2f28bcd0f3de8845409b947d9de45ca5ed776767': 'Multi Factor Validator',
-      '0x9fc07b42b4e13cbea040f4f3f80223c9adf4005e': 'Auto Savings Executor',
-      '0x506a89d85a9733225fdb54d9e7e76d017c21e1c1': 'Scheduled Orders Executor',
-      '0xad6330089d9a1fe89f4020292e1afe9969a5a2fc':
-        'Scheduled Transfers Executor',
-      '0xba313f2beee4118b97dd565ed451f722219851be': 'Cold Storage Executor',
-      '0xdfe507ad4035256e586f9e8b7d0e82184d5b8cb4': 'Cold Storage Hook',
-      '0x71181a93753d489a10927fa747eefd8425ff40de': 'Flashloan Callback',
-      '0x10a734a2c140a3d8e7919ad19c4d2f15f2f5f36b': 'Flashloan Lender',
-      '0xc5409e364bed768facd8ce3e534a9c7787c0ec3b': 'Session Key Manager',
     };
     for (const chain of CHAINS) {
       const chainAddresses = await getDeployed(chain, labels);
