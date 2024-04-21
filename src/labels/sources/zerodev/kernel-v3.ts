7e7a1e0f79262b508424bff6751c3391d5b684a4
Timur Badretdinov <accounts@destiner.io>
2024-04-21 21:05:13 +0100

    Tweak Kernel V3 source

--- src/labels/sources/zerodev/kernel-v3.ts
+++ src/labels/sources/zerodev/kernel-v3.ts
@@ -15,7 +15,7 @@ class Source extends BaseSource {
   async fetch(): Promise<LabelMap> {
     const addresses: Record<string, Record<Address, string>> = {};
     const labels: Record<Address, string> = {
-      '0xd703aae79538628d27099b8c4f621be4ccd142d5': 'Factory Staker',
+      '0xd703aae79538628d27099b8c4f621be4ccd142d5': 'Meta Factory',
       '0x94f097e1ebeb4eca3aae54cabb08905b239a7d27': 'Implementation',
       '0x6723b44abeec4e71ebe3232bd5b455805badd22f': 'Factory',
     };
