e192384f988019caf41692c7a1ec07a02c40ce63
Timur Badretdinov <accounts@destiner.io>
2024-05-12 06:03:26 +0100

    Update Uniswap V3 sources

--- src/labels/sources/uniswap/v3-pools.ts
+++ src/labels/sources/uniswap/v3-pools.ts
@@ -18,6 +18,12 @@ import {
   BASE,
   ARBITRUM,
   CELO,
+  OPTIMISM_SEPOLIA,
+  BLAST,
+  BASE_SEPOLIA,
+  AVALANCHE,
+  ARBITRUM_SEPOLIA,
+  SEPOLIA,
 } from '@/utils/chains.js';
 import type { ChainId } from '@/utils/chains.js';
 import { getEvents } from '@/utils/fetching.js';
@@ -109,12 +115,24 @@ class Source extends BaseSource {
         return '0xdb1d10011ad0ff90774d0c6bb92e5c5c8b4461f7';
       case POLYGON:
         return '0x1f98431c8ad98523631ae4a59f267346ea31f984';
+      case OPTIMISM_SEPOLIA:
+        return '0x8ce191193d15ea94e11d327b4c7ad8bbe520f6af';
       case BASE:
         return '0x33128a8fc17869897dce68ed026d694621f6fdfd';
+      case BLAST:
+        return '0x792edade80af5fc680d96a2ed80a44247d2cf6fd';
+      case BASE_SEPOLIA:
+        return '0x4752ba5dbc23f44d87826276bf6fd6b1c372ad24';
       case ARBITRUM:
         return '0x1f98431c8ad98523631ae4a59f267346ea31f984';
       case CELO:
         return '0xafe208a311b21f13ef87e33a90049fc17a7acdec';
+      case AVALANCHE:
+        return '0x740b1c1de25031c31ff4fc9a62f554a55cdc1bad';
+      case ARBITRUM_SEPOLIA:
+        return '0x248ab79bbb9bc29bb72f7cd42f17e054fc40188e';
+      case SEPOLIA:
+        return '0x0227628f3F023bb0B980b67D528571c95c6DaC1c';
       default:
         return null;
     }
