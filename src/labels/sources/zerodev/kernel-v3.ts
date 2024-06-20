9819164e22fec0295ffb66c16c5a24095e581732
Timur Badretdinov <accounts@destiner.io>
2024-06-20 23:53:07 +0100

    Update Kernel V3 source

--- src/labels/sources/zerodev/kernel-v3.ts
+++ src/labels/sources/zerodev/kernel-v3.ts
@@ -15,8 +15,10 @@ class Source extends BaseSource {
   async fetch(chain: ChainId): Promise<ChainSingleLabelMap> {
     const labels: Record<Address, string> = {
       '0xd703aae79538628d27099b8c4f621be4ccd142d5': 'Meta Factory',
-      '0x94f097e1ebeb4eca3aae54cabb08905b239a7d27': 'Implementation',
-      '0x6723b44abeec4e71ebe3232bd5b455805badd22f': 'Factory',
+      '0x94f097e1ebeb4eca3aae54cabb08905b239a7d27': 'Implementation V3.0',
+      '0x6723b44abeec4e71ebe3232bd5b455805badd22f': 'Factory V3.0',
+      '0xbac849bb641841b44e965fb01a4bf5f074f84b4d': 'Implementation V3.1',
+      '0xaac5d4240af87249b3f71bc8e4a2cae074a3e419': 'Factory V3.1',
     };
     const chainAddresses = await getDeployed(chain, labels);
     return toChainLabelMap(chainAddresses, 'zerodev-kernel-v3');
