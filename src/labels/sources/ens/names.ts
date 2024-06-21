7046ca68792dcc810f96f692a04b72084cdcbca6
Timur Badretdinov <accounts@destiner.io>
2024-06-21 03:58:56 +0100

    Update ENS name source
    Handle reverse registrar

--- src/labels/sources/ens/names.ts
+++ src/labels/sources/ens/names.ts
@@ -4,6 +4,7 @@ import ensEthRegistrarControllerAbi from '@/abi/ensEthRegistrarController';
 import ensLegacyEthRegistrarControllerAbi from '@/abi/ensLegacyEthRegistrarController';
 import ensLegacyPublicResolverAbi from '@/abi/ensLegacyPublicResolver';
 import ensPublicResolverAbi from '@/abi/ensPublicResolver';
+import ensReverseRegistrarAbi from '@/abi/ensReverseRegistrar';
 import {
   Source as BaseSource,
   ChainSingleLabelMap,
@@ -18,9 +19,12 @@ const ADDRESS_LEGACY_ETH_REGISTRAR =
   '0x283af0b28c62c092c9727f1ee09c02ca627eb7f5';
 const ADDRESS_LEGACY_PUBLIC_RESOLVER =
   '0x4976fb03c32e5b8cfe2b6ccb31c09ba78ebaba41';
+const ADDRESS_REVERSE_REGISTRAR = '0xa58e81fe9b61b5c3fe2afd33cf304c454abfc7cb';
 
 const TOPIC_NAME_RENEWED =
   '0x3da24c024582931cfaf8267d8ed24d13a82a8068d5bd337d30ec45cea4e506ae';
+const TOPIC_NAME_CHANGED =
+  '0xb7d29e911041e8d9b843369e890bcb72c9388692ba48b65ac54e7214c4c348f7';
 const TOPIC_ADDR_CHANGED =
   '0x52d7d861f09ab3d26239d492e8968629f95e9e318cf0b73bfddc441522a15fd2';
 const TOPIC_TEXT_CHANGED =
@@ -29,6 +33,8 @@ const TOPIC_NAME_REGISTERED =
   '0x69e37f151eb98a09618ddaa80c8cfaf1ce5996867c489f45b555b412271ebf27';
 const TOPIC_NAME_REGISTERED_LEGACY =
   '0xca6abbe9d7f11422cb6ca7629fbf6fe9efb1c621f71ce8f02b9f2a230097404f';
+const TOPIC_REVERSE_CLAIMED =
+  '0x6ada868dd3058cf77a48a74489fd7963688e5464b2b0fa957ace976243270e92';
 
 class Source extends BaseSource {
   getName(): string {
@@ -39,10 +45,27 @@ class Source extends BaseSource {
     if (chain !== ETHEREUM && chain !== SEPOLIA) {
       return {};
     }
+    const reverseClaimMap = await this.#getReverseClaimMap(chain);
     const labelHashMap = await this.#getLabelHashMap(chain);
     const addressMap = await this.#getAddressMap(chain);
     const avatarMap = await this.#getAvatarMap(chain);
     const labels: ChainSingleLabelMap = {};
+    // First priority: reverse claims
+    for (const addressString in reverseClaimMap) {
+      const address = addressString as Address;
+      const name = reverseClaimMap[address];
+      if (!name) {
+        continue;
+      }
+      const nameHash = namehash(name);
+      const avatar = avatarMap[nameHash];
+      const label: Label = {
+        value: name,
+        iconUrl: avatar,
+      };
+      labels[address] = label;
+    }
+    // Second priority: regular registrations
     for (const nodeString in labelHashMap) {
       const node = nodeString as Hex;
       const name = labelHashMap[node];
@@ -88,8 +111,8 @@ class Source extends BaseSource {
       TOPIC_NAME_RENEWED,
     );
 
-    const legacyNameRegistrations = legacyNameRegistrationEvents
-      .map((event) => {
+    const legacyNameRegistrations = legacyNameRegistrationEvents.map(
+      (event) => {
         const decodedEvent = decodeEventLog({
           abi: ensLegacyEthRegistrarControllerAbi,
           data: event.data,
@@ -102,71 +125,149 @@ class Source extends BaseSource {
           name: decodedEvent.args.name,
           expires: decodedEvent.args.expires,
         };
-      })
-      .filter((registration) => registration.expires > Date.now() / 1000)
-      .map((registration) => registration.name);
-    const legacyNameRenewals = legacyNameRenewalEvents
-      .map((event) => {
-        const decodedEvent = decodeEventLog({
-          abi: ensLegacyEthRegistrarControllerAbi,
-          data: event.data,
-          topics: event.topics as [Hex, ...Hex[]],
-        });
-        if (decodedEvent.eventName !== 'NameRenewed') {
-          throw new Error('Unexpected event name');
-        }
-        return {
-          name: decodedEvent.args.name,
-          expires: decodedEvent.args.expires,
-        };
-      })
-      .filter((renewal) => renewal.expires > Date.now() / 1000)
-      .map((renewal) => renewal.name);
-    const nameRegistrations = nameRegistrationEvents
-      .map((event) => {
-        const decodedEvent = decodeEventLog({
-          abi: ensEthRegistrarControllerAbi,
-          data: event.data,
-          topics: event.topics as [Hex, ...Hex[]],
-        });
-        if (decodedEvent.eventName !== 'NameRegistered') {
-          throw new Error('Unexpected event name');
-        }
-        return {
-          name: decodedEvent.args.name,
-          expires: decodedEvent.args.expires,
-        };
-      })
-      .filter((registration) => registration.expires > Date.now() / 1000)
-      .map((registration) => registration.name);
-    const nameRenewals = nameRenewalEvents
-      .map((event) => {
-        const decodedEvent = decodeEventLog({
-          abi: ensEthRegistrarControllerAbi,
-          data: event.data,
-          topics: event.topics as [Hex, ...Hex[]],
-        });
-        if (decodedEvent.eventName !== 'NameRenewed') {
-          throw new Error('Unexpected event name');
-        }
-        return {
-          name: decodedEvent.args.name,
-          expires: decodedEvent.args.expires,
-        };
-      })
-      .filter((renewal) => renewal.expires > Date.now() / 1000)
-      .map((renewal) => renewal.name);
+      },
+    );
+    const legacyNameRenewals = legacyNameRenewalEvents.map((event) => {
+      const decodedEvent = decodeEventLog({
+        abi: ensLegacyEthRegistrarControllerAbi,
+        data: event.data,
+        topics: event.topics as [Hex, ...Hex[]],
+      });
+      if (decodedEvent.eventName !== 'NameRenewed') {
+        throw new Error('Unexpected event name');
+      }
+      return {
+        name: decodedEvent.args.name,
+        expires: decodedEvent.args.expires,
+      };
+    });
+    const nameRegistrations = nameRegistrationEvents.map((event) => {
+      const decodedEvent = decodeEventLog({
+        abi: ensEthRegistrarControllerAbi,
+        data: event.data,
+        topics: event.topics as [Hex, ...Hex[]],
+      });
+      if (decodedEvent.eventName !== 'NameRegistered') {
+        throw new Error('Unexpected event name');
+      }
+      return {
+        name: decodedEvent.args.name,
+        expires: decodedEvent.args.expires,
+      };
+    });
+    const nameRenewals = nameRenewalEvents.map((event) => {
+      const decodedEvent = decodeEventLog({
+        abi: ensEthRegistrarControllerAbi,
+        data: event.data,
+        topics: event.topics as [Hex, ...Hex[]],
+      });
+      if (decodedEvent.eventName !== 'NameRenewed') {
+        throw new Error('Unexpected event name');
+      }
+      return {
+        name: decodedEvent.args.name,
+        expires: decodedEvent.args.expires,
+      };
+    });
+    const registrationList = [
+      ...legacyNameRegistrations,
+      ...legacyNameRenewals,
+      ...nameRegistrations,
+      ...nameRenewals,
+    ];
+    // Store the most recent registration for each name
+    // But preserve the order of name registrations to get the first name for each address
+    const names: Record<string, bigint> = {};
+    for (const registration of registrationList) {
+      const name = registration.name;
+      const expires = registration.expires;
+      const previousExpires = names[name];
+      if (!previousExpires || previousExpires < expires) {
+        names[name] = expires;
+      }
+    }
     const map: Record<Hex, string> = Object.fromEntries(
-      legacyNameRegistrations
-        .concat(legacyNameRenewals)
-        .concat(nameRegistrations)
-        .concat(nameRenewals)
-        .map((name) => {
+      Object.entries(names)
+        .filter(([, expires]) => expires > Date.now() / 1000)
+        .map(([name]) => {
           const fullName = `${name}.eth`;
           return [namehash(fullName), fullName];
         }),
     );
     return map;
+  }
+
+  async #getReverseClaimMap(chain: ChainId): Promise<Record<Address, string>> {
+    const reverseClaimedEvents = await getEvents(
+      chain,
+      ADDRESS_REVERSE_REGISTRAR,
+      TOPIC_REVERSE_CLAIMED,
+    );
+    const reverseClaims = reverseClaimedEvents.map((event) => {
+      const decodedEvent = decodeEventLog({
+        abi: ensReverseRegistrarAbi,
+        data: event.data,
+        topics: event.topics as [Hex, ...Hex[]],
+      });
+      if (decodedEvent.eventName !== 'ReverseClaimed') {
+        throw new Error('Unexpected event name');
+      }
+      return {
+        node: decodedEvent.args.node,
+        address: decodedEvent.args.addr.toLowerCase() as Address,
+      };
+    });
+
+    const legacyNameChangedEvents = await getEvents(
+      chain,
+      ADDRESS_LEGACY_PUBLIC_RESOLVER,
+      TOPIC_NAME_CHANGED,
+    );
+    const nameChangedEvents = await getEvents(
+      chain,
+      ADDRESS_PUBLIC_RESOLVER,
+      TOPIC_NAME_CHANGED,
+    );
+    const legacyNameChanges = legacyNameChangedEvents.map((event) => {
+      const decodedEvent = decodeEventLog({
+        abi: ensLegacyPublicResolverAbi,
+        data: event.data,
+        topics: event.topics as [Hex, ...Hex[]],
+      });
+      if (decodedEvent.eventName !== 'NameChanged') {
+        throw new Error('Unexpected event name');
+      }
+      return {
+        node: decodedEvent.args.node,
+        name: decodedEvent.args.name,
+      };
+    });
+    const nameChanges = nameChangedEvents.map((event) => {
+      const decodedEvent = decodeEventLog({
+        abi: ensPublicResolverAbi,
+        data: event.data,
+        topics: event.topics as [Hex, ...Hex[]],
+      });
+      if (decodedEvent.eventName !== 'NameChanged') {
+        throw new Error('Unexpected event name');
+      }
+      return {
+        node: decodedEvent.args.node,
+        name: decodedEvent.args.name,
+      };
+    });
+    const nameMap = Object.fromEntries(
+      legacyNameChanges
+        .concat(nameChanges)
+        .map((change) => [change.node, change.name]),
+    );
+
+    return Object.fromEntries(
+      reverseClaims.map((claim) => [
+        claim.address,
+        nameMap[claim.node] as string,
+      ]),
+    );
   }
 
   async #getAddressMap(chain: ChainId): Promise<Record<Hex, Address>> {
