1ca43df6e4a4c63735daa62f7762523446eabd77
Timur Badretdinov <accounts@destiner.io>
2024-06-24 09:16:52 +0100

    Add DB table indexes

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,35 +1,63 @@
-import { integer, text, sqliteTable } from 'drizzle-orm/sqlite-core';
+import { integer, text, sqliteTable, index } from 'drizzle-orm/sqlite-core';
 
-const labels = sqliteTable('labels', {
-  id: integer('id').primaryKey(),
-  chain: integer('chain').notNull(),
-  address: text('address').notNull(),
-  value: text('value').notNull(),
-  typeId: text('typeId'),
-  namespaceId: text('namespaceId'),
-  iconUrl: text('iconUrl'),
-});
+const labels = sqliteTable(
+  'labels',
+  {
+    id: integer('id').primaryKey(),
+    chain: integer('chain').notNull(),
+    address: text('address').notNull(),
+    value: text('value').notNull(),
+    typeId: text('typeId'),
+    namespaceId: text('namespaceId'),
+    iconUrl: text('iconUrl'),
+  },
+  (table) => {
+    return {
+      idx: index('idx_labels').on(table.chain, table.address, table.typeId),
+    };
+  },
+);
 
-const logs = sqliteTable('logs', {
-  id: integer('id').primaryKey(),
-  chain: integer('chain').notNull(),
-  address: text('address').notNull(),
-  blockNumber: integer('blockNumber').notNull(),
-  logIndex: integer('logIndex').notNull(),
-  topic0: text('topics'),
-  topic1: text('topics'),
-  topic2: text('topics'),
-  topic3: text('topics'),
-  data: text('data'),
-});
+const logs = sqliteTable(
+  'logs',
+  {
+    id: integer('id').primaryKey(),
+    chain: integer('chain').notNull(),
+    address: text('address').notNull(),
+    blockNumber: integer('blockNumber').notNull(),
+    logIndex: integer('logIndex').notNull(),
+    topic0: text('topics'),
+    topic1: text('topics'),
+    topic2: text('topics'),
+    topic3: text('topics'),
+    data: text('data'),
+  },
+  (table) => {
+    return {
+      idx: index('idx_logs').on(table.chain, table.address, table.topic0),
+    };
+  },
+);
 
-const logsMetadata = sqliteTable('logs_metadata', {
-  id: integer('id').primaryKey(),
-  chain: integer('chain').notNull(),
-  address: text('address').notNull(),
-  topic0: text('topics'),
-  latestBlockNumber: integer('latestBlockNumber').notNull(),
-});
+const logsMetadata = sqliteTable(
+  'logs_metadata',
+  {
+    id: integer('id').primaryKey(),
+    chain: integer('chain').notNull(),
+    address: text('address').notNull(),
+    topic0: text('topics'),
+    latestBlockNumber: integer('latestBlockNumber').notNull(),
+  },
+  (table) => {
+    return {
+      idx: index('idx_logs_metadata').on(
+        table.chain,
+        table.address,
+        table.topic0,
+      ),
+    };
+  },
+);
 
 type Label = typeof labels.$inferInsert;
 
