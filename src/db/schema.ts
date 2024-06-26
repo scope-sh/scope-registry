36adfb9101c3e2eed6eee44ad9f8f36562337f0e
Timur Badretdinov <accounts@destiner.io>
2024-06-26 06:05:41 +0100

    Remove logs and logs metadata tables from DB schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -24,49 +24,8 @@ const labelSearch = sqliteTable('label_search', {
   value: text('value').notNull(),
 });
 
-const logs = sqliteTable(
-  'logs',
-  {
-    id: integer('id').primaryKey(),
-    chain: integer('chain').notNull(),
-    address: text('address').notNull(),
-    blockNumber: integer('blockNumber').notNull(),
-    logIndex: integer('logIndex').notNull(),
-    topic0: text('topic0'),
-    topic1: text('topic1'),
-    topic2: text('topic2'),
-    topic3: text('topic3'),
-    data: text('data'),
-  },
-  (table) => {
-    return {
-      idx: index('idx_logs').on(table.chain, table.address, table.topic0),
-    };
-  },
-);
-
-const logsMetadata = sqliteTable(
-  'logs_metadata',
-  {
-    id: integer('id').primaryKey(),
-    chain: integer('chain').notNull(),
-    address: text('address').notNull(),
-    topic0: text('topic0'),
-    latestBlockNumber: integer('latestBlockNumber').notNull(),
-  },
-  (table) => {
-    return {
-      idx: index('idx_logs_metadata').on(
-        table.chain,
-        table.address,
-        table.topic0,
-      ),
-    };
-  },
-);
-
 type Label = typeof labels.$inferInsert;
 type LabelSearch = typeof labelSearch.$inferInsert;
 
-export { labels, labelSearch, logs, logsMetadata };
+export { labels, labelSearch };
 export type { Label, LabelSearch };
