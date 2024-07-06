a27e66212c7c845d05b65bbbfd971507b8519122
Timur Badretdinov <accounts@destiner.io>
2024-07-06 15:19:06 +0100

    Add contract data insertion helper functions

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,7 +3,20 @@ import { NodePgDatabase, drizzle } from 'drizzle-orm/node-postgres';
 import pg from 'pg';
 import { Address, Hex } from 'viem';
 
-import { type Label as LabelValues, labels as tableLabels } from '@/db/schema';
+import {
+  type ContractCode as ContractCodeValues,
+  type Contract as ContractValues,
+  type ContractDeployment as ContractDeploymentValues,
+  type CompiledContract as CompiledContractValues,
+  type VerifiedContract as VerifiedContractValues,
+  type Label as LabelValues,
+  labels as tableLabels,
+  contractCode as tableContractCode,
+  contracts as tableContracts,
+  contractDeployments as tableContractDeployments,
+  compiledContracts as tableCompiledContracts,
+  verifiedContracts as tableVerifiedContracts,
+} from '@/db/schema';
 import { Label } from '@/index.js';
 
 import { ChainId } from './chains';
@@ -56,6 +69,111 @@ async function addLabels(
   }
 }
 
+async function addContractCodes(codes: ContractCodeValues[]): Promise<void> {
+  const db = getDb();
+  const batchSize = 5_000;
+  const batchCount = Math.ceil(codes.length / batchSize);
+  for (let i = 0; i < batchCount; i++) {
+    const batch = codes.slice(i * batchSize, (i + 1) * batchSize);
+    const codeBatch: ContractCodeValues[] = batch.map((code) => {
+      return code;
+    });
+    await db.insert(tableContractCode).values(codeBatch).execute();
+  }
+}
+
+async function addContracts(contracts: ContractValues[]): Promise<void> {
+  const db = getDb();
+  const batchSize = 5_000;
+  const batchCount = Math.ceil(contracts.length / batchSize);
+  for (let i = 0; i < batchCount; i++) {
+    const batch = contracts.slice(i * batchSize, (i + 1) * batchSize);
+    const contractBatch: ContractValues[] = batch.map((contract) => {
+      return contract;
+    });
+    await db
+      .insert(tableContracts)
+      .values(contractBatch)
+      .onConflictDoNothing()
+      .execute();
+  }
+}
+
+async function addContractDeployments(
+  contractDeployments: ContractDeploymentValues[],
+): Promise<void> {
+  const db = getDb();
+  const batchSize = 1;
+  const batchCount = Math.ceil(contractDeployments.length / batchSize);
+  for (let i = 0; i < batchCount; i++) {
+    const batch = contractDeployments.slice(i * batchSize, (i + 1) * batchSize);
+    const contractDeploymentBatch: ContractDeploymentValues[] = batch.map(
+      (contractDeployment) => {
+        return contractDeployment;
+      },
+    );
+    try {
+      await db
+        .insert(tableContractDeployments)
+        .values(contractDeploymentBatch)
+        .onConflictDoNothing()
+        .execute();
+    } catch (e) {
+      console.log('Error adding contract deployments');
+    }
+  }
+}
+
+async function addCompiledContracts(
+  compiledContracts: CompiledContractValues[],
+): Promise<void> {
+  const db = getDb();
+  const batchSize = 1;
+  const batchCount = Math.ceil(compiledContracts.length / batchSize);
+  for (let i = 0; i < batchCount; i++) {
+    const batch = compiledContracts.slice(i * batchSize, (i + 1) * batchSize);
+    const compiledContractBatch: CompiledContractValues[] = batch.map(
+      (compiledContract) => {
+        return compiledContract;
+      },
+    );
+    try {
+      await db
+        .insert(tableCompiledContracts)
+        .values(compiledContractBatch)
+        .onConflictDoNothing()
+        .execute();
+    } catch (e) {
+      console.log('Error adding compiled contracts');
+    }
+  }
+}
+
+async function addVerifiedContracts(
+  verifiedContracts: VerifiedContractValues[],
+): Promise<void> {
+  const db = getDb();
+  const batchSize = 1;
+  const batchCount = Math.ceil(verifiedContracts.length / batchSize);
+  for (let i = 0; i < batchCount; i++) {
+    const batch = verifiedContracts.slice(i * batchSize, (i + 1) * batchSize);
+    const verifiedContractBatch: VerifiedContractValues[] = batch.map(
+      (verifiedContract) => {
+        return verifiedContract;
+      },
+    );
+    try {
+      await db
+        .insert(tableVerifiedContracts)
+        .values(verifiedContractBatch)
+        .onConflictDoNothing()
+        .execute();
+    } catch (e) {
+      console.log('Error adding verified contracts');
+    }
+  }
+}
+
 async function disconnect(): Promise<void> {
   await client.end();
 }
@@ -64,5 +182,14 @@ function getDb(): NodePgDatabase {
   return drizzle(client);
 }
 
-export { removeLabels, addLabels, disconnect };
+export {
+  removeLabels,
+  addLabels,
+  addContractCodes,
+  addContracts,
+  addContractDeployments,
+  addCompiledContracts,
+  addVerifiedContracts,
+  disconnect,
+};
 export type { LabelWithAddress, Log };
