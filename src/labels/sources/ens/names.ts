5104a5b0b7c862ec7c70398a0e663b321ab68683
Timur Badretdinov <accounts@destiner.io>
2024-08-27 17:56:07 +0100

    Update ENS names source
    Remove non-primary names

--- src/labels/sources/ens/names.ts
+++ src/labels/sources/ens/names.ts
@@ -3,8 +3,6 @@ import { join } from 'node:path';
 
 import { Address, Hex, decodeEventLog, namehash, size } from 'viem';
 
-import ensEthRegistrarControllerAbi from '@/abi/ensEthRegistrarController';
-import ensLegacyEthRegistrarControllerAbi from '@/abi/ensLegacyEthRegistrarController';
 import ensLegacyPublicResolverAbi from '@/abi/ensLegacyPublicResolver';
 import ensPublicResolverAbi from '@/abi/ensPublicResolver';
 import ensReverseRegistrarAbi from '@/abi/ensReverseRegistrar';
@@ -18,18 +16,12 @@ import { ChainId, ETHEREUM, SEPOLIA, getChainData } from '@/utils/chains';
 import { getLogs, Log } from '@/utils/fetching';
 import { getObject, putObject } from '@/utils/storage';
 
-const TOPIC_NAME_RENEWED =
-  '0x3da24c024582931cfaf8267d8ed24d13a82a8068d5bd337d30ec45cea4e506ae';
 const TOPIC_NAME_CHANGED =
   '0xb7d29e911041e8d9b843369e890bcb72c9388692ba48b65ac54e7214c4c348f7';
 const TOPIC_ADDRESS_CHANGED =
   '0x65412581168e88a1e60c6459d7f44ae83ad0832e670826c05a4e2476b57af752';
 const TOPIC_TEXT_CHANGED =
   '0x448bc014f1536726cf8d54ff3d6481ed3cbc683c2591ca204274009afa09b1a1';
-const TOPIC_NAME_REGISTERED =
-  '0x69e37f151eb98a09618ddaa80c8cfaf1ce5996867c489f45b555b412271ebf27';
-const TOPIC_NAME_REGISTERED_LEGACY =
-  '0xca6abbe9d7f11422cb6ca7629fbf6fe9efb1c621f71ce8f02b9f2a230097404f';
 const TOPIC_REVERSE_CLAIMED =
   '0x6ada868dd3058cf77a48a74489fd7963688e5464b2b0fa957ace976243270e92';
 
@@ -53,14 +45,12 @@ class Source extends BaseSource {
     const ensChain = getEnsChain(chain);
     const reverseClaimMap = await this.#getReverseClaimMap(ensChain);
     console.log('fetch 1');
-    const labelHashMap = await this.#getLabelHashMap(ensChain);
-    console.log('fetch 2');
     const addressMap = await this.#getAddressMap(ensChain);
-    console.log('fetch 3');
+    console.log('fetch 2');
     const avatarMap = await this.#getAvatarMap(ensChain);
-    console.log('fetch 4');
+    console.log('fetch 3');
     const labels: ChainSingleLabelMap = {};
-    // First priority: reverse claims
+    // Start with the legacy claims
     const legacyReverseMapString = await readFile(
       join(__dirname, './legacyReverseMap.json'),
       'utf-8',
@@ -112,166 +102,7 @@ class Source extends BaseSource {
       };
       labels[address] = label;
     }
-    // Second priority: regular registrations
-    for (const nodeString in labelHashMap) {
-      const node = nodeString as Hex;
-      const name = labelHashMap[node];
-      if (!name) {
-        continue;
-      }
-      const chainMap = addressMap[node] || ({} as Record<ChainId, Address>);
-      const chains = Object.keys(chainMap).map(
-        (chain) => parseInt(chain) as ChainId,
-      );
-      chains.sort((a, b) => {
-        // First priority: the chain we are fetching for
-        if (a === chain) return -1;
-        if (b === chain) return 1;
-        // Second priority: Ethereum
-        if (a === ETHEREUM) return -1;
-        if (b === ETHEREUM) return 1;
-        // Third priority: other chains
-        return a - b;
-      });
-      const topChain = chains[0];
-      if (!topChain) {
-        continue;
-      }
-      const address = chainMap[topChain];
-      if (!address) {
-        continue;
-      }
-      if (labels[address]) {
-        continue;
-      }
-      const avatar = avatarMap[node];
-      const label: Label = {
-        value: name,
-        sourceId: this.getInfo().id,
-        indexed: false,
-        iconUrl: avatar,
-      };
-      labels[address] = label;
-    }
     return labels;
-  }
-
-  async #getLabelHashMap(ensChain: ChainId): Promise<Record<Hex, string>> {
-    const legacyEthRegistrarAddress = getLegacyEthRegistrarAddress(ensChain);
-    const ethRegistrarAddress = getEthRegistrarAddress(ensChain);
-    const legacyNameRegistrationLogs = await getLogs(
-      this.getInfo(),
-      ensChain,
-      legacyEthRegistrarAddress,
-      TOPIC_NAME_REGISTERED_LEGACY,
-    );
-    const legacyNameRenewalLogs = await getLogs(
-      this.getInfo(),
-      ensChain,
-      legacyEthRegistrarAddress,
-      TOPIC_NAME_RENEWED,
-    );
-    const nameRegistrationLogs = await getLogs(
-      this.getInfo(),
-      ensChain,
-      ethRegistrarAddress,
-      TOPIC_NAME_REGISTERED,
-    );
-    const nameRenewalLogs = await getLogs(
-      this.getInfo(),
-      ensChain,
-      ethRegistrarAddress,
-      TOPIC_NAME_RENEWED,
-    );
-
-    const legacyNameRegistrations = legacyNameRegistrationLogs.map((log) => {
-      const decodedLog = decodeEventLog({
-        abi: ensLegacyEthRegistrarControllerAbi,
-        data: log.data,
-        topics: log.topics as [Hex, ...Hex[]],
-      });
-      if (decodedLog.eventName !== 'NameRegistered') {
-        throw new Error('Unexpected event name');
-      }
-      return {
-        block: log.blockNumber,
-        name: decodedLog.args.name,
-        expires: decodedLog.args.expires,
-      };
-    });
-    const legacyNameRenewals = legacyNameRenewalLogs.map((log) => {
-      const decodedLog = decodeEventLog({
-        abi: ensLegacyEthRegistrarControllerAbi,
-        data: log.data,
-        topics: log.topics as [Hex, ...Hex[]],
-      });
-      if (decodedLog.eventName !== 'NameRenewed') {
-        throw new Error('Unexpected event name');
-      }
-      return {
-        block: log.blockNumber,
-        name: decodedLog.args.name,
-        expires: decodedLog.args.expires,
-      };
-    });
-    const nameRegistrations = nameRegistrationLogs.map((log) => {
-      const decodedLog = decodeEventLog({
-        abi: ensEthRegistrarControllerAbi,
-        data: log.data,
-        topics: log.topics as [Hex, ...Hex[]],
-      });
-      if (decodedLog.eventName !== 'NameRegistered') {
-        throw new Error('Unexpected event name');
-      }
-      return {
-        block: log.blockNumber,
-        name: decodedLog.args.name,
-        expires: decodedLog.args.expires,
-      };
-    });
-    const nameRenewals = nameRenewalLogs.map((log) => {
-      const decodedLog = decodeEventLog({
-        abi: ensEthRegistrarControllerAbi,
-        data: log.data,
-        topics: log.topics as [Hex, ...Hex[]],
-      });
-      if (decodedLog.eventName !== 'NameRenewed') {
-        throw new Error('Unexpected event name');
-      }
-      return {
-        block: log.blockNumber,
-        name: decodedLog.args.name,
-        expires: decodedLog.args.expires,
-      };
-    });
-    const registrationList = [
-      ...legacyNameRegistrations,
-      ...legacyNameRenewals,
-      ...nameRegistrations,
-      ...nameRenewals,
-    ];
-    registrationList.sort((a, b) => a.block - b.block);
-    // Store the most recent registration for each name
-    // But preserve the order of name registrations to get the first name for each address
-    const names = await getNameExpiryMapCache(ensChain);
-    for (const registration of registrationList) {
-      const name = registration.name;
-      const expires = registration.expires;
-      const previousExpires = names[name];
-      if (!previousExpires || previousExpires < expires) {
-        names[name] = parseInt(expires.toString());
-      }
-    }
-    await setNameExpiryMapCache(ensChain, names);
-    const map: Record<Hex, string> = Object.fromEntries(
-      Object.entries(names)
-        .filter(([, expires]) => expires > Date.now() / 1000)
-        .map(([name]) => {
-          const fullName = `${name}.eth`;
-          return [namehash(fullName), fullName];
-        }),
-    );
-    return map;
   }
 
   async #getReverseClaimMap(
@@ -467,19 +298,6 @@ class Source extends BaseSource {
   }
 }
 
-async function getNameExpiryMapCache(
-  chain: ChainId,
-): Promise<Record<string, number>> {
-  return getCache(chain, 'nameExpiryMap');
-}
-
-async function setNameExpiryMapCache(
-  chain: ChainId,
-  map: Record<string, number>,
-): Promise<void> {
-  return setCache(chain, 'nameExpiryMap', map);
-}
-
 async function getReverseClaimMapCache(
   chain: ChainId,
 ): Promise<Record<Hex, Address>> {
@@ -573,34 +391,12 @@ async function setCache<K extends string, V>(
   await putObject(cacheKey, JSON.stringify(cache));
 }
 
-function getEthRegistrarAddress(chain: ChainId): Address {
-  switch (chain) {
-    case ETHEREUM:
-      return '0x253553366da8546fc250f225fe3d25d0c782303b';
-    case SEPOLIA:
-      return '0xfed6a969aaa60e4961fcd3ebf1a2e8913ac65b72';
-    default:
-      throw new Error('Unsupported chain');
-  }
-}
-
 function getPublicResolverAddress(chain: ChainId): Address {
   switch (chain) {
     case ETHEREUM:
       return '0x231b0ee14048e9dccd1d247744d114a4eb5e8e63';
     case SEPOLIA:
       return '0x8fade66b79cc9f707ab26799354482eb93a5b7dd';
-    default:
-      throw new Error('Unsupported chain');
-  }
-}
-
-function getLegacyEthRegistrarAddress(chain: ChainId): Address {
-  switch (chain) {
-    case ETHEREUM:
-      return '0x283af0b28c62c092c9727f1ee09c02ca627eb7f5';
-    case SEPOLIA:
-      return '0x7e02892cfc2bfd53a75275451d73cf620e793fc0';
     default:
       throw new Error('Unsupported chain');
   }
