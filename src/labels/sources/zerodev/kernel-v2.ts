c97441b0c217ede48a8f3d902771e984957f0c38
Timur Badretdinov <accounts@destiner.io>
2024-09-04 19:34:29 +0100

    Update Kernel V2 source

--- src/labels/sources/zerodev/kernel-v2.ts
+++ src/labels/sources/zerodev/kernel-v2.ts
@@ -24,7 +24,11 @@ class Source extends BaseSource {
 
   async fetch(chain: ChainId): Promise<ChainSingleLabelMap> {
     const labels: Record<Address, string> = {
-      '0xd3082872f8b06073a021b4602e022d5a070d7cfc': 'Implementation',
+      '0xd3082872f8b06073a021b4602e022d5a070d7cfc': 'Implementation V2.4',
+      '0xd3f582f6b4814e989ee8e96bc3175320b5a540ab': 'Implementation V2.3',
+      '0x0da6a956b9488ed4dd761e59f52fdc6c8068e6b5': 'Implementation V2.2',
+      '0xf048ad83cb2dfd6037a43902a2a5be04e53cd2eb': 'Implementation V2.1',
+      '0xeb8206e02f6ab1884cfea58cc7babda7d55ac957': 'Implementation V2.0',
       '0x5de4839a76cf55d0c90e2061ef4386d962e15ae3': 'Factory',
       '0xb8e3c4beaacad06f6092793012da4a8cb23d6123': 'Session Key Validator',
       '0xd9ab5096a832b9ce79914329daee236f8eea0390': 'ECDSA Validator',
