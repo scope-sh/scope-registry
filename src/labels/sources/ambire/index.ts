fd8cae80515ef0a999c2ace4f44784418062ba9d
Timur Badretdinov <accounts@destiner.io>
2025-03-23 14:00:42 +0000

    fix: update Ambire

--- src/labels/sources/ambire/index.ts
+++ src/labels/sources/ambire/index.ts
@@ -18,6 +18,7 @@ class Source extends BaseSource {
   async fetch(chain: ChainId): Promise<ChainSingleLabelMap> {
     const contracts: Record<Address, string> = {
       '0x0317e77f08cfcf137ef9d7b517e236f8175f2f7e': '7702 Account',
+      '0x5a7fc11397e9a8ad41bf10bf13f22b0a63f96f6d': '7702 Account',
       '0xa8b267c68715fa1dca055993149f30217b572cf0': 'Paymaster',
     };
     const chainAddresses = await getDeployed(chain, contracts);
